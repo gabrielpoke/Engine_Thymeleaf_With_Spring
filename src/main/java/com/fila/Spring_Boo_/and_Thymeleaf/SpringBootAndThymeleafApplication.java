1503e4e733e640ed73b97638692601dcc34e96f5
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-09-21 20:16:13 -0300

    Pirmeiro commit

---
+++ src/main/java/com/fila/Spring_Boo_/and_Thymeleaf/SpringBootAndThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.fila.Spring_Boo_.and_Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fila/Spring_Boo_/and_Thymeleaf/SpringBootAndThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fila.Spring_Boo_.and_Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
