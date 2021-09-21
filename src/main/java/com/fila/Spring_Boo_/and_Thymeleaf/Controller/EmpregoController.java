5ae0d5a7b0eee1c021e95b2de0a68393fc0ed9c4
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-09-21 20:52:24 -0300

    Criando controllers

---
+++ src/main/java/com/fila/Spring_Boo_/and_Thymeleaf/Controller/EmpregoController.java
@@ -0,0 +1,50 @@
+package com.fila.Spring_Boo_.and_Thymeleaf.Controller;
+
+
+import com.fila.Spring_Boo_.and_Thymeleaf.Model.Emprego;
+import com.fila.Spring_Boo_.and_Thymeleaf.Repository.EmpregoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class EmpregoController {
+
+    @Autowired
+    EmpregoRepository repository;
+
+    @RequestMapping("/")
+    public String listaEmpregos(Model model){
+
+        model.addAttribute("empregos", repository.findAll());
+
+        return "list";
+
+    }
+
+    @GetMapping("/add")
+    public String empregoForm(Model model){
+
+        model.addAttribute("emprego", new Emprego());
+
+        return "empregoForm";
+
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Emprego emprego, BindingResult result){
+        if(result.hasErrors()){
+            return "empregoForm";
+        }
+
+        repository.save(emprego);
+
+        return "redirect:/";
+    }
+}
